978ead0bdf8a80e99d2a5882e3ceae31e3b14a1c
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2020-04-21 09:37:54 +0200

    Added GUI to select and save files

--- traffic_parser.py
+++ traffic_parser.py
@@ -1,17 +1,14 @@
-import csv, re, sys
+import tkinter as tk, csv, sys, re
+from tkinter import filedialog
 
-if len(sys.argv) == 1:
-    print('File to parse not specified.')
-    sys.exit(1)
-
-inputFile = sys.argv[1]
-
-with open(inputFile + '_parsed.csv', mode='w', newline='') as trafico_parsed:
+inputFile = filedialog.askopenfilename(title = "Select Fortigate log",filetypes = (("CSV Files","*.csv"),))
+output_path = filedialog.asksaveasfile(mode='a', defaultextension=".csv", filetypes = (("CSV Files","*.csv"),))
+with open(output_path.name, mode='w', newline='') as trafico_parsed:
     csv_writer = csv.writer(trafico_parsed, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     with open(inputFile, mode='r') as traffic_file:
         csv_reader = csv.DictReader(traffic_file, delimiter=';')
-        columns = ['devname', 'srcip', 'srcport', 'srcname', 'srcintf','devtype', 'osname', 'dstip', 'dstport', 'dstintf', 'service', 'policyid']
+        columns = ['date', 'time', 'devname', 'srcip', 'srcport', 'srcname', 'srcintf','devtype', 'osname', 'dstip', 'dstport', 'dstintf', 'service', 'policyid']
 
         line_count = 0
         for row in csv_reader:
